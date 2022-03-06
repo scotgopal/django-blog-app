7323939ded977f7638d28e3bcfca92a4f21df395
Scotrraaj <scotgopal@gmail.com>
2022-03-07 01:42:14 +0800

    Initial commit

---
+++ blog/views.py
@@ -0,0 +1,14 @@
+from django.views.generic import ListView, DetailView
+from .models import Post
+
+# Create your views here.
+class BlogListView(ListView):
+    model = Post
+    template_name = "home.html"
+    context_object_name = "all_posts_list"
+
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = "post_detail.html"
+    context_object_name = "post"
